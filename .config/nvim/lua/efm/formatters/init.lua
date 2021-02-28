09fd48bee681483fc015c3b2e24a0eff097484f4
Veselin Ivanov <v3s1ez@gmail.com>
2021-02-28 19:29:43 +0200

    chore(vim): splitting linters and formatters

---
+++ .config/nvim/lua/efm/formatters/init.lua
@@ -0,0 +1,4 @@
+return {
+    luaFormat = require('efm/formatters/lua-format'),
+    prettier = require('efm/formatters/prettier')
+}
---
+++ .config/nvim/lua/efm/linters/init.lua
@@ -0,0 +1,4 @@
+return {
+    eslint_d = require('efm/linters/eslint_d'),
+    markdownlint = require('efm/linters/markdownlint')
+}
