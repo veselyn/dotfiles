5e3f9d1bb15956848f865f3da33f62536dfc68e9
Veselin Ivanov <v3s1ez@gmail.com>
2021-05-11 17:48:54 +0200

    fix(vim): remove timeout from formatting_sync

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -52,7 +52,7 @@ local on_attach = function(client, bufnr)
 
     if client.resolved_capabilities.document_formatting then
         vim.api.nvim_command(
-            'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)')
+            'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
     end
 
     if client.resolved_capabilities.document_highlight then
