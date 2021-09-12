0b5cd0b6e16d43f7d0080eac0d4d37db1be89c5e
Veselin Ivanov <v3s1ez@gmail.com>
2021-09-12 15:37:18 +0200

    feat(vim): add cmp mappings

--- config/nvim/lua/completion/init.lua
+++ config/nvim/lua/completion/init.lua
@@ -2,7 +2,16 @@ local cmp = require('cmp')
 
 cmp.setup({
     -- snippet = {},
-    -- mapping = {},
+    mapping = {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        })
+    }
     -- sources = {}
 })
 
