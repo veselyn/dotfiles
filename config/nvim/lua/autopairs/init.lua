3841708ba48bd0a4d67e464444bde28c0344d2eb
Veselin Ivanov <v3s1ez@gmail.com>
2021-09-12 16:59:43 +0200

    feat(vim): map cr in autopairs

--- config/nvim/lua/autopairs/init.lua
+++ config/nvim/lua/autopairs/init.lua
@@ -1,3 +1,7 @@
 local autopairs = require('nvim-autopairs')
 
+local cmp = require('nvim-autopairs/completion/cmp')
+
 autopairs.setup()
+
+cmp.setup({map_cr = true, map_complete = true, auto_select = true})
