2fa4f10e2d9d592f5a6ac0162b441f0c2ac86ae7
Veselin Ivanov <v3s1ez@gmail.com>
2021-09-12 15:38:00 +0200

    chore(vim): increase scroll docs to 5

--- config/nvim/lua/completion/init.lua
+++ config/nvim/lua/completion/init.lua
@@ -3,8 +3,8 @@ local cmp = require('cmp')
 cmp.setup({
     -- snippet = {},
     mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-5),
+        ['<C-f>'] = cmp.mapping.scroll_docs(5),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({
