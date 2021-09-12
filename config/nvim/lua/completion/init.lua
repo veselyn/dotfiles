2a2b15a4e897ff874031d6f7d773cad1a0018e81
Veselin Ivanov <v3s1ez@gmail.com>
2021-09-12 16:17:43 +0200

    feat(vim): supertab cmp

--- config/nvim/lua/completion/init.lua
+++ config/nvim/lua/completion/init.lua
@@ -2,6 +2,22 @@ local cmp = require('cmp')
 
 local luasnip = require('luasnip')
 
+local feedkey = function(key)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true),
+                          'n', true)
+end
+
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+        return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and
+               vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col,
+                                                                          col)
+                   :match('%s') == nil
+end
+
 cmp.setup({
     completion = {autocomplete = false},
     snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
@@ -13,7 +29,27 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
-        })
+        }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                feedkey('<C-n>')
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                feedkey('<C-p>')
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'})
     },
     sources = {
         {name = 'path'}, {name = 'buffer'}, {name = 'tags'}, {name = 'spell'},
