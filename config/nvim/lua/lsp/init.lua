4a8d0d996e9d0d9131f9c70d75892d3fb3f233e3
Veselin Ivanov <v3s1ez@gmail.com>
2021-09-15 20:49:03 +0200

    chore(vim): call on_attach first

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -110,8 +110,8 @@ local configs = {
     },
     typescript = {
         on_attach = function(client, bufnr)
-            client.resolved_capabilities.document_formatting = false
             on_attach(client, bufnr)
+            client.resolved_capabilities.document_formatting = false
         end
     }
 }
