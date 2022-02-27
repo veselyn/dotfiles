94d23f8ad78eda831ae4d3f8a27c84f9668f6252
Veselin Ivanov <v3s1ez@gmail.com>
2022-02-28 00:11:03 +0100

    refactor(yabaiscript): move services

---
+++ bin/_yabaiscript/services.sh
@@ -0,0 +1,27 @@
+services() {
+  case $1 in
+  start | stop | restart)
+    case $2 in
+    yabai | skhd)
+      "_$1" "$2"
+      ;;
+    *)
+      "_$1" yabai
+      "_$1" skhd
+      ;;
+    esac
+    ;;
+  esac
+}
+
+_start() {
+  brew services start "$1"
+}
+
+_stop() {
+  brew services stop "$1"
+}
+
+_restart() {
+  brew services restart "$1"
+}
