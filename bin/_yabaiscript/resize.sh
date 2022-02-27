39403f82d16ac97fd2af46b0f40361e8b67b818f
Veselin Ivanov <v3s1ez@gmail.com>
2022-02-27 20:48:53 +0100

    refactor(yabai): move resize

---
+++ bin/_yabaiscript/resize.sh
@@ -0,0 +1,68 @@
+resize() {
+  case $1 in
+  left)
+    _resize_left
+    ;;
+  bottom)
+    _resize_bottom
+    ;;
+  top)
+    _resize_top
+    ;;
+  right)
+    _resize_right
+    ;;
+  esac
+}
+
+_is_floating() {
+  is_floating=$(yabai -m query --windows --window | jq '."is-floating"')
+
+  if [ "$is_floating" = false ]; then
+    return 1
+  fi
+
+  if [ "$is_floating" = true ]; then
+    return 0
+  fi
+}
+
+_resize_left() {
+  if _is_floating; then
+    yabai -m window --move rel:-10:0
+    return
+  fi
+
+  yabai -m window --resize left:-10:0
+  yabai -m window --resize right:-10:0
+}
+
+_resize_bottom() {
+  if _is_floating; then
+    yabai -m window --move rel:0:10
+    return
+  fi
+
+  yabai -m window --resize bottom:0:10
+  yabai -m window --resize top:0:10
+}
+
+_resize_top() {
+  if _is_floating; then
+    yabai -m window --move rel:0:-10
+    return
+  fi
+
+  yabai -m window --resize top:0:-10
+  yabai -m window --resize bottom:0:-10
+}
+
+_resize_right() {
+  if _is_floating; then
+    yabai -m window --move rel:10:0
+    return
+  fi
+
+  yabai -m window --resize right:10:0
+  yabai -m window --resize left:10:0
+}
