796f8d95816db5680bbe156a4f88659da1643307
Veselin Ivanov <v3s1ez@gmail.com>
2022-05-23 11:43:10 +0200

    fix(zsh): change gwip alias to commit without [skip ci] in msg

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -3,5 +3,6 @@ alias vd='vimdiff'
 alias r='ranger'
 
 alias gai='git add --interactive'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip--"'
 
 alias yay='brew update && brew upgrade'
