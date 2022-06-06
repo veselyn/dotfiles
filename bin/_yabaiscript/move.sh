540b67ea093605c02eeae3c27708cd875f909da6
Veselin Ivanov <v3s1ez@gmail.com>
2022-06-06 15:42:24 +0200

    chore(yabaiscript): move

---
+++ bin/_yabaiscript/move.sh
@@ -0,0 +1,23 @@
+move() {
+  case $1 in
+  left | bottom | top | right)
+    "_move_$1"
+    ;;
+  esac
+}
+
+_move_left() {
+  yabai -m window --move rel:-10:0
+}
+
+_move_bottom() {
+  yabai -m window --move rel:0:10
+}
+
+_move_top() {
+  yabai -m window --move rel:0:-10
+}
+
+_move_right() {
+  yabai -m window --move rel:10:0
+}
