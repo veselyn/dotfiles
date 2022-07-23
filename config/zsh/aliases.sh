793fc73c9bc9645275073b0b5d1b8732057ab535
Veselin Ivanov <v3s1ez@gmail.com>
2022-07-23 14:11:34 +0300

    feat(zsh): alias c and p to pbcopy and pbpaste respectively

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -8,3 +8,5 @@ alias gai='git add --interactive'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip--"'
 
 alias yay='brew update && brew upgrade'
+alias c='pbcopy'
+alias p='pbpaste'
