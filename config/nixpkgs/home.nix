43717f924957aae182135c49a4dab0f72dc39ca0
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-25 23:37:34 +0200

    fix(nixpkgs): configure vim darwin support

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -86,6 +86,10 @@
       };
       vim = {
         enable = true;
+        packageConfigurable = pkgs.vim_configurable.override {
+          guiSupport = false;
+          darwinSupport = true;
+        };
         extraConfig = ''
           source ${config.home.file.".vim".source}/vimrc
         '';
