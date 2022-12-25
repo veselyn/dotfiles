9bfb44a4eaecea4ce1c49905273c11f4e7d0f2a0
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-25 17:20:07 +0200

    refactor(nixpkgs): group home configuration

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -9,19 +9,10 @@
   };
 
   home-manager.users.veselin = {
-    # Home Manager needs a bit of information about you and the
-    # paths it should manage.
-    home.username = "veselin";
-    home.homeDirectory = "/Users/veselin";
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "22.11";
+    home = {
+      username = "veselin";
+      homeDirectory = "/Users/veselin";
+      stateVersion = "22.11";
+    };
   };
 }
