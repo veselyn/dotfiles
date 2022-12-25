0fe4951da963f61e381d85fb7151f5179d60a0a3
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-25 17:20:12 +0200

    refactor(nixpkgs): format configuration.nix with lsp

--- config/nixpkgs/darwin/configuration.nix
+++ config/nixpkgs/darwin/configuration.nix
@@ -15,7 +15,8 @@
 
   homebrew.enable = true;
   homebrew.casks =
-    [ "1password"
+    [
+      "1password"
       "firefox"
     ];
 
@@ -28,7 +29,8 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
-    [ pkgs._1password
+    [
+      pkgs._1password
       pkgs.fzf
       pkgs.vim-full
     ];
@@ -42,7 +44,7 @@
   # nix.package = pkgs.nix;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
