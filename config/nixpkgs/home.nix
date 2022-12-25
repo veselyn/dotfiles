89c7431420be57d41c34e4974e5d45dbee64dceb
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-25 17:20:08 +0200

    feat(nixpkgs): symlink yabai configuration

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -8,11 +8,16 @@
     home = "/Users/veselin";
   };
 
-  home-manager.users.veselin = {
+  home-manager.users.veselin = { config, pkgs, ...}: {
     home = {
       username = "veselin";
       homeDirectory = "/Users/veselin";
       stateVersion = "22.11";
+    };
+    xdg = {
+      configFile = {
+        yabai.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/source/private/dotfiles/config/yabai";
+      };
     };
   };
 }
