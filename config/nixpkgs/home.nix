e159936dc3d9be4be5bae666f439e9e58c8e764e
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-25 17:20:11 +0200

    feat(nixpkgs): enable zsh autosuggestions, completion and syntax highlighting

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -26,6 +26,9 @@
     programs = {
       zsh = {
         enable = true;
+        enableAutosuggestions = true;
+        enableCompletion = true;
+        enableSyntaxHighlighting = true;
         oh-my-zsh = {
           enable = true;
           theme = "robbyrussell";
