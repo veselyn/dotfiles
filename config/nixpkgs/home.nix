f519bb4361d12bcec653390d8b15d76a717add52
Veselin Ivanov <v3s1ez@gmail.com>
2022-12-26 00:01:08 +0200

    feat(nixpkgs): define XDG_* session variables

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -23,6 +23,10 @@
         ];
       sessionVariables = {
         EDITOR = "vim";
+        XDG_CACHE_HOME = config.xdg.cacheHome;
+        XDG_CONFIG_HOME = config.xdg.configHome;
+        XDG_DATA_HOME = config.xdg.dataHome;
+        XDG_STATE_HOME = config.xdg.stateHome;
       };
     };
     programs = {
