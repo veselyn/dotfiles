147c09fa96a5a306dad4ab4ede3d0f45d77b1bb6
Veselin Ivanov <v3s1ez@gmail.com>
2023-01-04 08:07:33 +0100

    refactor(nixpkgs): clean up darwin programs

--- config/nixpkgs/darwin/configuration.nix
+++ config/nixpkgs/darwin/configuration.nix
@@ -151,7 +151,9 @@
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+    };
+  };
 }
