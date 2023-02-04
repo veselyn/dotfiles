1630fa6e8544aef5a10fca0d1acf3138a835a234
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-04 22:37:09 +0100

    feat(flake): set darwin nixpkgs input to follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { darwin, ... }: {
