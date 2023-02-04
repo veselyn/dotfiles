21e455b21bc59d549cf392bd24939bc08db60687
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-04 22:53:10 +0100

    feat(flake): set darwin and home-manager input branch to master explicitly

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    darwin.url = "github:lnl7/nix-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
