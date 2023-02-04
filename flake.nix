9e0b4f30143534e5cb5b74cfc9375f385cd9979d
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-04 22:37:56 +0100

    feat(flake): add home.nix to darwin modules

--- flake.nix
+++ flake.nix
@@ -5,14 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { darwin, ... }: {
+  outputs = { darwin, home-manager, ... }: {
     darwinConfigurations = {
       veselins-macbook-pro = darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [
           ./config/nixpkgs/darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          ./config/nixpkgs/home.nix
         ];
       };
     };
