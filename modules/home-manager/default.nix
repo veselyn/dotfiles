6363464a3b70bdfa53dda7f809ab669fd0a02aa7
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-18 17:29:32 +0100

    refactor(home): move tmux program into module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -76,37 +76,6 @@
     };
   };
   programs = {
-    tmux = {
-      enable = true;
-      baseIndex = 1;
-      clock24 = true;
-      customPaneNavigationAndResize = true;
-      escapeTime = 0;
-      keyMode = "vi";
-      shortcut = "a";
-      terminal = "xterm-kitty";
-      plugins =
-        [
-          pkgs.tmuxPlugins.vim-tmux-navigator
-        ];
-      extraConfig = ''
-        set -g mouse on
-        set -g status off
-
-        bind C-a send C-a
-        bind C-h send C-h
-        bind C-j send C-j
-        bind C-k send C-k
-        bind C-l send C-l
-        bind C-\\ send C-\\
-
-        bind c new-window -c "#{pane_current_path}"
-        bind - split-window -c "#{pane_current_path}"
-        bind \\ split-window -h -c "#{pane_current_path}"
-        bind _ split-window -f -c "#{pane_current_path}"
-        bind | split-window -f -h -c "#{pane_current_path}"
-      '';
-    };
     fzf = {
       enable = true;
     };
