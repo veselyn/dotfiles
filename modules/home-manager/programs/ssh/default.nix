086e079c91c42691159a6a16bd32a62fc24156df
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-18 17:40:55 +0100

    refactor(home): move ssh program into module

---
+++ modules/home-manager/programs/ssh/default.nix
@@ -0,0 +1,17 @@
+_:
+
+{
+  programs.ssh = {
+    enable = true;
+    forwardAgent = true;
+    matchBlocks = {
+      "*" = {
+        extraOptions = {
+          IgnoreUnknown = "UseKeychain";
+          AddKeysToAgent = "yes";
+          UseKeychain = "yes";
+        };
+      };
+    };
+  };
+}
