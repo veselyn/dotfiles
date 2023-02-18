de23f67b8ca3c456426d8205b8f0b9f15108b7bd
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-18 17:17:57 +0100

    refactor(darwin): move yabai service into module

---
+++ modules/darwin/services/yabai/default.nix
@@ -0,0 +1,15 @@
+_:
+
+{
+  services.yabai = {
+    enable = true;
+    config = {
+      layout = "bsp";
+      left_padding = 5;
+      bottom_padding = 5;
+      top_padding = 5;
+      right_padding = 5;
+      window_gap = 5;
+    };
+  };
+}
