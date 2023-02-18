98ed48ce98894779ad9d6352fc6ac4b7bcc8aa0a
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-18 17:36:44 +0100

    refactor(home): move vim program into module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -76,16 +76,6 @@
     };
   };
   programs = {
-    vim = {
-      enable = true;
-      packageConfigurable = pkgs.vim_configurable.override {
-        guiSupport = false;
-        darwinSupport = true;
-      };
-      extraConfig = ''
-        source ${config.home.file.".vim".source}/vimrc
-      '';
-    };
     gpg = {
       enable = true;
     };
