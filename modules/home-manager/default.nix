e43b0b653b70813120bf98ea27e1f64fc04efd8d
Veselin Ivanov <v3s1ez@gmail.com>
2023-02-18 17:39:34 +0100

    refactor(home): move git program into module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -76,25 +76,6 @@
     };
   };
   programs = {
-    git = {
-      enable = true;
-      userName = "Veselin Ivanov";
-      userEmail = "v3s1ez@gmail.com";
-      signing = {
-        key = "B09DDF9D424E10D3";
-        signByDefault = true;
-      };
-      extraConfig = {
-        merge = {
-          tool = "vimdiff";
-        };
-      };
-      ignores =
-        [
-          ".vimrc"
-          "Session.vim"
-        ];
-    };
     ssh = {
       enable = true;
       forwardAgent = true;
