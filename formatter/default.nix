1a7cf6ddee2aee347f0912731dedf2fd446ea61d
Veselin Ivanov <v3s1ez@gmail.com>
2023-04-13 17:42:15 +0300

    refactor(flake/formatter): extract to new file

---
+++ formatter/default.nix
@@ -0,0 +1,10 @@
+{ self, nixpkgs, flake-utils, ... }:
+
+flake-utils.lib.eachDefaultSystemMap (system:
+let
+  pkgs = import nixpkgs {
+    inherit system; overlays = builtins.attrValues self.overlays;
+  };
+in
+pkgs.nixpkgs-fmt
+)
