d0df4ffc0b7fcee2c49a4b06349252aaab73d49c
Veselin Ivanov <v3s1ez@gmail.com>
2023-04-17 19:48:20 +0300

    refactor(darwin): move system to its own file

---
+++ modules/darwin/system.nix
@@ -0,0 +1,36 @@
+_:
+
+{
+  system = {
+    defaults = {
+      CustomUserPreferences = {
+        "com.apple.Accessibility" = {
+          ReduceMotionEnabled = 1;
+        };
+      };
+      NSGlobalDomain = {
+        "com.apple.keyboard.fnState" = true;
+        AppleKeyboardUIMode = 3;
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        _HIHideMenuBar = true;
+      };
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+        tilesize = 16;
+      };
+      magicmouse = {
+        MouseButtonMode = "TwoButton";
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      nonUS = {
+        remapTilde = true;
+      };
+      remapCapsLockToEscape = true;
+    };
+    stateVersion = 4;
+  };
+}
