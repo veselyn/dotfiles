7f7afaacb973999ff222b9c82bf8e248fbde7573
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-01 21:13:58 +0200

    feat(darwin/services): move skhd config out of yabaiscript

--- bin/_yabaiscript/grid.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-grid() {
-	case $1 in
-	center)
-		yabai -m window --grid 4:4:1:1:2:2
-		;;
-	esac
-}
--- bin/_yabaiscript/toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-toggle() {
-	case $1 in
-	zoom-parent | zoom-fullscreen | split | float)
-		yabai -m window --toggle "$1"
-		;;
-	esac
-}
