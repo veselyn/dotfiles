718a95c66b43645f49c4df9b32f74b81b78fc16d
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-05 12:41:25 +0200

    refactor: reformat apps and packages

--- apps/default.nix
+++ apps/default.nix
@@ -4,15 +4,13 @@
   flake-utils,
   ...
 }:
-flake-utils.lib.eachDefaultSystemMap (
-  system: let
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = builtins.attrValues self.overlays;
-    };
-    scripts = pkgs.callPackage ../scripts {};
-  in {
-    battery = flake-utils.lib.mkApp {drv = scripts.battery;};
-    flushdns = flake-utils.lib.mkApp {drv = scripts.flushdns;};
-  }
-)
+flake-utils.lib.eachDefaultSystemMap (system: let
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = builtins.attrValues self.overlays;
+  };
+  scripts = pkgs.callPackage ../scripts {};
+in {
+  battery = flake-utils.lib.mkApp {drv = scripts.battery;};
+  flushdns = flake-utils.lib.mkApp {drv = scripts.flushdns;};
+})
