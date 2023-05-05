c7292e850506a353ef29248902c8d8a4a815eed5
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-05 20:49:14 +0200

    feat(pkgs): init yabai at 5.0.4

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,8 @@ lib.makeScope pkgs.newScope (self:
       battery = callPackage ./battery {};
       flushdns = callPackage ./flushdns {};
       yabaictl = callPackage ./yabaictl {};
+
+      yabai = callPackage ./yabai {
+        inherit (darwin.apple_sdk_11_0.frameworks) SkyLight Cocoa Carbon ScriptingBridge;
+      };
     })
