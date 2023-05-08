7bd12940575b594e79574d1daf0eb4892809166f
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-08 20:05:57 +0200

    feat(home): clean up bin directory link and path append

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -7,11 +7,8 @@
     stateVersion = "22.11";
 
     file = {
-      "bin".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/bin";
       ".vim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/vim";
     };
-
-    sessionPath = ["${config.home.file."bin".source}"];
 
     shellAliases = {
       ls = "ls --color=auto";
