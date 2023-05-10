dc82ccb13ccbb4aff8c9c885bdd1b5c26d4ced70
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-10 08:40:47 +0200

    feat(overlays): add nixpkgs-master to overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,10 @@
-_: {
+{nixpkgs-master, ...}: {
   default = final: prev: let
+    master = import nixpkgs-master {system = prev.system;};
     pkgs = prev.callPackages ../pkgs {};
   in {
+    inherit master;
+
     inherit
       (pkgs)
       battery
