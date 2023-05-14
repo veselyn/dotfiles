e314c97797d2044777498f974066b901962dcf35
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-14 19:41:11 +0200

    feat(overlays): inherit stable pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,14 @@
-{nixpkgs-master, ...}: {
+{
+  nixpkgs-stable,
+  nixpkgs-master,
+  ...
+}: {
   default = final: prev: let
+    stable = import nixpkgs-stable {inherit (prev) system;};
     master = import nixpkgs-master {inherit (prev) system;};
     pkgs = prev.callPackages ../pkgs {};
   in {
+    inherit stable;
     inherit master;
 
     inherit
