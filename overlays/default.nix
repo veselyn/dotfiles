cfab480d69a875ad45ded8885bb4ac84b062100b
Veselin Ivanov <v3s1ez@gmail.com>
2023-05-22 17:51:46 +0200

    feat(overlays): remove yabai overwrite

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,7 +15,6 @@
       (pkgs)
       battery
       flushdns
-      yabai
       yabaictl
       ;
   };
--- packages/default.nix
+++ packages/default.nix
@@ -14,7 +14,6 @@ in {
     (pkgs)
     battery
     flushdns
-    yabai
     yabaictl
     ;
 })
