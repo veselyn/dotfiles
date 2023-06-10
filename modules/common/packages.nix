784499c86ac3bb70eb2654c68e5ce39552f4dc30
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-10 18:15:27 +0200

    refactor(common/packages): declare packages directly in attrset

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -1,52 +1,48 @@
 {pkgs, ...}:
-with pkgs; let
-  coreutils = [
-    coreutils-full
-    findutils
-    findutils.locate
-    gnugrep
-    gnumake
-    gnused
-    gnutar
-    inetutils
-    time
-  ];
-  scripts = [
-    battery
-    flushdns
-    yabaictl
-  ];
-  others = [
-    _1password
-    awscli2
-    buf
-    cargo-watch
-    cloc
-    docker
-    fd
-    go
-    gotools
-    grc
-    highlight
-    httpie
-    kubectl
-    kubectx
-    mkcert
-    neofetch
-    nodejs
-    parallel
-    python3
-    ripgrep
-    sd
-    silver-searcher
-    sshuttle
-    terraform
-    watch
-  ];
-in {
+with pkgs; {
   environment.systemPackages = lib.flatten (builtins.attrValues {
-    inherit coreutils;
-    inherit scripts;
-    inherit others;
+    coreutils = [
+      coreutils-full
+      findutils
+      findutils.locate
+      gnugrep
+      gnumake
+      gnused
+      gnutar
+      inetutils
+      time
+    ];
+    scripts = [
+      battery
+      flushdns
+      yabaictl
+    ];
+    others = [
+      _1password
+      awscli2
+      buf
+      cargo-watch
+      cloc
+      docker
+      fd
+      go
+      gotools
+      grc
+      highlight
+      httpie
+      kubectl
+      kubectx
+      mkcert
+      neofetch
+      nodejs
+      parallel
+      python3
+      ripgrep
+      sd
+      silver-searcher
+      sshuttle
+      terraform
+      watch
+    ];
   });
 }
