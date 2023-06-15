e64206ff223b261c15632d019f083b646db7b673
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-15 19:48:40 +0200

    feat(nvim): set scrolloff and sidescrolloff

--- nvim/plugin/opt.lua
+++ nvim/plugin/opt.lua
@@ -4,6 +4,8 @@ vim.opt.list = true
 vim.opt.listchars:append({ extends = ">", precedes = "<" })
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 1
 vim.opt.shortmess:append("I")
+vim.opt.sidescrolloff = 2
 vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
