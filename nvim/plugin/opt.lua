bde05dad6392e90432729e76bbec9645441e1e24
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-15 19:48:07 +0200

    feat(nvim): set listchars for extends and precedes
    This is done by vim-sensible also.

--- nvim/plugin/opt.lua
+++ nvim/plugin/opt.lua
@@ -1,6 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.guicursor = nil
 vim.opt.list = true
+vim.opt.listchars:append({ extends = ">", precedes = "<" })
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shortmess:append("I")
