8a1fe508885b566b4c178176a74839b5a894d4a5
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-18 16:22:26 +0200

    refactor(nvim): sort telescope keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
 
+vim.keymap.set("n", "<Leader>fb", builtin.buffers)
 vim.keymap.set("n", "<Leader>ff", builtin.find_files)
 vim.keymap.set("n", "<Leader>fg", builtin.live_grep)
-vim.keymap.set("n", "<Leader>fb", builtin.buffers)
 vim.keymap.set("n", "<Leader>fh", builtin.help_tags)
