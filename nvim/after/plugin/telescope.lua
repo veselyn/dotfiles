e24d7cdb8b243a5186e59887198c45e68114c778
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-18 17:40:07 +0200

    feat(nvim): add keymaps for Telescope pickers and resume pickers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,3 +1,13 @@
+local telescope = require("telescope")
+
+telescope.setup({
+	defaults = {
+		cache_picker = {
+			num_pickers = 10,
+		},
+	},
+})
+
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<Leader>f#", builtin.grep_string)
@@ -7,3 +17,5 @@ vim.keymap.set("n", "<Leader>ff", builtin.find_files)
 vim.keymap.set("n", "<Leader>fg", builtin.live_grep)
 vim.keymap.set("n", "<Leader>fh", builtin.help_tags)
 vim.keymap.set("n", "<Leader>fj", builtin.git_files)
+vim.keymap.set("n", "<Leader>fp", builtin.pickers)
+vim.keymap.set("n", "<Leader>fr", builtin.resume)
