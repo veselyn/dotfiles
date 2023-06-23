869f0c088467cbfde3f5f109b2955dda36bcfb26
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-23 19:57:10 +0200

    feat(nvim): enable regex sytax highlight for gitcommit filetype

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -3,7 +3,7 @@ local configs = require("nvim-treesitter.configs")
 configs.setup({
 	autotag = { enable = true },
 	endwise = { enable = true },
-	highlight = { enable = true },
+	highlight = { enable = true, additional_vim_regex_highlighting = { "gitcommit" } },
 	incremental_selection = { enable = true },
 	textobjects = { enable = true },
 })
