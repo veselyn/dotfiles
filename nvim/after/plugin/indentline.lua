f0da456de2cea2458a974250c578830e012f7079
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-24 16:00:20 +0200

    refactor(nvim): assign indentline require to local variable

--- nvim/after/plugin/indentline.lua
+++ nvim/after/plugin/indentline.lua
@@ -1,4 +1,6 @@
-require("indent_blankline").setup({
+local indentline = require("indent_blankline")
+
+indentline.setup({
 	show_current_context = true,
 	show_current_context_start = true,
 	show_current_context_start_on_current_line = false,
