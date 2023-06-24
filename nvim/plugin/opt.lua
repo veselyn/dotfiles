929b71ec7f57ce994585e54a2fcecd150d83c59a
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-24 18:01:07 +0200

    feat(nvim): use ignorecase with smart options

--- nvim/plugin/opt.lua
+++ nvim/plugin/opt.lua
@@ -3,6 +3,7 @@ vim.opt.backupdir:remove(".")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = false
 vim.opt.guicursor = nil
+vim.opt.ignorecase = true
 vim.opt.lazyredraw = true
 vim.opt.list = true
 vim.opt.listchars:append({ extends = ">", precedes = "<" })
@@ -14,6 +15,9 @@ vim.opt.shiftwidth = 4
 vim.opt.shortmess:append("I")
 vim.opt.sidescrolloff = 2
 vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
 vim.opt.swapfile = true
 vim.opt.tabstop = 4
 vim.opt.undofile = true
