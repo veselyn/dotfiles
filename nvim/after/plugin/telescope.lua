2b72484c3bb80b0929d739ed4545df9bd2d44118
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-24 16:35:41 +0200

    feat(nvim): use fullscreen ivy theme with telescope pickers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+local themes = require("telescope.themes")
 
 telescope.setup({
 	defaults = {
@@ -9,12 +10,18 @@ telescope.setup({
 	},
 })
 
-vim.keymap.set("n", "<Leader>f#", builtin.grep_string)
-vim.keymap.set("n", "<Leader>f*", builtin.grep_string)
-vim.keymap.set("n", "<Leader>fb", builtin.buffers)
-vim.keymap.set("n", "<Leader>ff", builtin.find_files)
-vim.keymap.set("n", "<Leader>fg", builtin.live_grep)
-vim.keymap.set("n", "<Leader>fh", builtin.help_tags)
-vim.keymap.set("n", "<Leader>fj", builtin.git_files)
-vim.keymap.set("n", "<Leader>fp", builtin.pickers)
-vim.keymap.set("n", "<Leader>fr", builtin.resume)
+local function map(mode, lhs, rhs, opts)
+	vim.keymap.set(mode, lhs, function()
+		rhs(themes.get_ivy({ layout_config = { height = 100 } }))
+	end, opts)
+end
+
+map("n", "<Leader>f#", builtin.grep_string)
+map("n", "<Leader>f*", builtin.grep_string)
+map("n", "<Leader>fb", builtin.buffers)
+map("n", "<Leader>ff", builtin.find_files)
+map("n", "<Leader>fg", builtin.live_grep)
+map("n", "<Leader>fh", builtin.help_tags)
+map("n", "<Leader>fj", builtin.git_files)
+map("n", "<Leader>fp", builtin.pickers)
+map("n", "<Leader>fr", builtin.resume)
