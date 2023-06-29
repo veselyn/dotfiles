107fabaf3040cb20a618d26556c73efa57707c68
Veselin Ivanov <v3s1ez@gmail.com>
2023-06-29 21:31:21 +0200

    feat(nvim): setup format on save for null

--- nvim/after/plugin/null.lua
+++ nvim/after/plugin/null.lua
@@ -4,4 +4,15 @@ null.setup({
 	sources = {
 		null.builtins.formatting.stylua,
 	},
+	on_attach = function(_, bufnr)
+		local augroup = vim.api.nvim_create_augroup("LspFormat", { clear = false })
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end,
 })
