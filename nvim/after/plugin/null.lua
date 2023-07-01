fa6771dc89d83fa2958cb94f76615a160accf5af
Veselin Ivanov <v3s1ez@gmail.com>
2023-07-01 13:04:50 +0200

    feat(nvim): setup alejandra formatting tool

--- nvim/after/plugin/null.lua
+++ nvim/after/plugin/null.lua
@@ -3,6 +3,7 @@ local format_on_save = require("aul.lsp.format")
 
 null.setup({
 	sources = {
+		null.builtins.formatting.alejandra,
 		null.builtins.formatting.stylua,
 	},
 	on_attach = function(_, bufnr)
