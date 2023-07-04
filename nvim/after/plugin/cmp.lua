f40f909c32a56ae539bdae53eb7f0ccffec2d0c9
Veselin Ivanov <v3s1ez@gmail.com>
2023-07-04 22:52:10 +0200

    refactor(nvim): sort cmp setup tab

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -5,15 +5,15 @@ cmp.setup({
 	completion = {
 		autocomplete = false,
 	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-D>"] = cmp.mapping.scroll_docs(1),
+		["<C-U>"] = cmp.mapping.scroll_docs(-1),
+	}),
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
 		end,
 	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-D>"] = cmp.mapping.scroll_docs(1),
-		["<C-U>"] = cmp.mapping.scroll_docs(-1),
-	}),
 	sources = cmp.config.sources({
 		{ name = "path" },
 		{ name = "nvim_lsp" },
