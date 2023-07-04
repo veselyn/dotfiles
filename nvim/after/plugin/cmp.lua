5433794366667060d59d96908b3afd91e31a8153
Veselin Ivanov <v3s1ez@gmail.com>
2023-07-04 22:17:50 +0200

    feat(nvim): set up luasnip as snippet engine for cmp

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,6 +1,12 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 	mapping = cmp.mapping.preset.insert(),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
