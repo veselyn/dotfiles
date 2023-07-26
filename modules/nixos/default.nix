2e0d7fea124c9f0571dc91fe6453382feb2fe89e
Veselin Ivanov <v3s1ez@gmail.com>
2023-07-27 00:47:41 +0300

    feat(nixos): set default shell to zsh for veselin

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running `nixos-help`).
-_: {
+{pkgs, ...}: {
   imports = [
     ../common
     # Include the results of the hardware scan.
@@ -15,6 +15,7 @@ _: {
   users.users.veselin = {
     isNormalUser = true;
     extraGroups = ["wheel"];
+    shell = pkgs.zsh;
   };
 
   # networking.hostName = "nixos"; # Define your hostname.
