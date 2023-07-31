f3f278b61a3c7fc280da2124a8c8e095fa305d98
Veselin Ivanov <v3s1ez@gmail.com>
2023-07-31 13:01:42 +0300

    feat(common): add nixpkgs to registry

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,6 +1,16 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   nix = {
     package = pkgs.nix;
     settings.experimental-features = ["nix-command" "flakes"];
+
+    registry = {
+      nixpkgs = {
+        flake = inputs.nixpkgs;
+      };
+    };
   };
 }
