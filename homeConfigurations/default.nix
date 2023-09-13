e4390c93f275bad9fcbfbd1315ad84fad9d2c210
Veselin Ivanov <me@veselyn.com>
2023-09-13 15:40:32 +0200

    feat(homeConfigurations): extract configuration to mkConfiguration

--- homeConfigurations/default.nix
+++ homeConfigurations/default.nix
@@ -3,35 +3,35 @@
   nixpkgs,
   self,
   ...
-} @ inputs: {
-  "veselin@aarch64-darwin" = home-manager.lib.homeManagerConfiguration {
-    pkgs = import nixpkgs {
-      system = "aarch64-darwin";
-      overlays = builtins.attrValues self.overlays;
-      config.allowUnfree = true;
-    };
-
-    extraSpecialArgs = {inherit inputs;};
-
-    modules = [
+} @ inputs: let
+  mkConfiguration = {
+    system,
+    user,
+    baseModules ? [
       ../modules/home
-      {modules.home.user = "veselin";}
+      {modules.home.user = user;}
       ../modules/common
-    ];
+    ],
+    extraModules ? [],
+  }:
+    home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+        config.allowUnfree = true;
+      };
+
+      extraSpecialArgs = {inherit inputs;};
+
+      modules = baseModules ++ extraModules;
+    };
+in {
+  "veselin@aarch64-darwin" = mkConfiguration {
+    system = "aarch64-darwin";
+    user = "veselin";
   };
-  "veselin@aarch64-linux" = home-manager.lib.homeManagerConfiguration {
-    pkgs = import nixpkgs {
-      system = "aarch64-linux";
-      overlays = builtins.attrValues self.overlays;
-      config.allowUnfree = true;
-    };
-
-    extraSpecialArgs = {inherit inputs;};
-
-    modules = [
-      ../modules/home
-      {modules.home.user = "veselin";}
-      ../modules/common
-    ];
+  "veselin@aarch64-linux" = mkConfiguration {
+    system = "aarch64-linux";
+    user = "veselin";
   };
 }
