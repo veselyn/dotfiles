fe780b973f5ea331488c554f534c8e2ecb451a85
Veselin Ivanov <me@veselyn.com>
2023-10-23 21:22:12 +0200

    refactor(home): reuse darwin and linux module

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -1,28 +1,5 @@
-{
-  config,
-  lib,
-  specialArgs,
-  ...
-}: {
-  options = with lib; {
-    modules.home = {
-      user = mkOption {};
-    };
-  };
-
-  config = let
-    cfg = config.modules.home;
-  in {
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-
-      extraSpecialArgs = specialArgs;
-
-      users.${cfg.user} = {
-        imports = [./standalone.nix];
-        modules.home.user = cfg.user;
-      };
-    };
-  };
+_: {
+  imports = [
+    ./module.nix
+  ];
 }
