2e75f35ebb22e65f03cfa1add3f1620d9d61711d
Veselin Ivanov <me@veselyn.com>
2023-11-03 10:48:42 +0100

    feat(nvim): migrate indentline to ibl.scope

--- modules/home/programs/neovim/nvim/after/plugin/indentline.lua
+++ modules/home/programs/neovim/nvim/after/plugin/indentline.lua
@@ -1,7 +1,3 @@
 local indentline = require("ibl")
 
-indentline.setup({
-	show_current_context = true,
-	show_current_context_start = true,
-	show_current_context_start_on_current_line = false,
-})
+indentline.setup()
