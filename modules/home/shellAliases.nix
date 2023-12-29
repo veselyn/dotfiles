5137e4413d4748d5264cef13cf9b5661f22e9ffb
Veselin Ivanov <me@veselyn.com>
2023-12-29 15:49:31 +0200

    feat(home/shellAliases): add pbcopy/pbpaste aliases

--- modules/home/shellAliases.nix
+++ modules/home/shellAliases.nix
@@ -1,8 +1,10 @@
 _: {
   home.shellAliases = {
+    c = "pbcopy";
     dsf = "diff-so-fancy";
     gai = "git add --interactive";
     ls = "ls --color=auto";
+    p = "pbpaste";
     s = "echo $SHLVL";
     v = "vim";
     vd = "vimdiff";
