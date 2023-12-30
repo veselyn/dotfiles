da109ccf318e2940e1669e7dbd730febada4e696
Veselin Ivanov <me@veselyn.com>
2023-12-30 16:07:59 +0000

    feat(home/linux): set vim keybindings in i3

--- modules/home/linux/i3.nix
+++ modules/home/linux/i3.nix
@@ -1,10 +1,37 @@
-_: {
+{
+  config,
+  lib,
+  ...
+}: {
   xsession.windowManager.i3 = {
     enable = true;
 
     config = {
       modifier = "Mod4";
       terminal = "kitty";
+
+      keybindings = let
+        inherit (config.xsession.windowManager.i3.config) modifier;
+      in
+        lib.mkOptionDefault {
+          "${modifier}+Shift+j" = "move left";
+          "${modifier}+Shift+k" = "move down";
+          "${modifier}+Shift+l" = "move up";
+          "${modifier}+Shift+semicolon" = "move right";
+          "${modifier}+j" = "focus left";
+          "${modifier}+k" = "focus down";
+          "${modifier}+l" = "focus up";
+          "${modifier}+semicolon" = "focus right";
+        };
+
+      modes = lib.mkOptionDefault {
+        resize = {
+          j = "resize shrink width 10 px or 10 ppt";
+          k = "resize grow height 10 px or 10 ppt";
+          l = "resize shrink height 10 px or 10 ppt";
+          semicolon = "resize grow width 10 px or 10 ppt";
+        };
+      };
     };
   };
 }
