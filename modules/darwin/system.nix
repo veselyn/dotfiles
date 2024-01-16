2efc5963faf90487871263ceb9c21c80fb972120
Veselin Ivanov <me@veselyn.com>
2024-01-16 22:04:37 +0100

    feat(darwin/system): increase key repeat frequency and decrease delay

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -2,6 +2,8 @@ _: {
   system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
   system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
   system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+  system.defaults.NSGlobalDomain.KeyRepeat = 2;
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
 }
