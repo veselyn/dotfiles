52a7681a24691aa6c56444839f4c5793e27d66e6
Veselin Ivanov <me@veselyn.com>
2024-01-16 22:04:27 +0100

    feat(home/darwin): disable press and hold

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,6 +1,7 @@
 _: {
   system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
   system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
 }
