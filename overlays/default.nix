f2a9b773e8864b51cd776c99baf39573b411cda7
Veselin Ivanov <me@veselyn.com>
2024-01-17 23:07:25 +0100

    feat(overlays): clean up oh-my-zsh override

--- overlays/default.nix
+++ overlays/default.nix
@@ -17,12 +17,5 @@
       flushdns
       yabaictl
       ;
-
-    oh-my-zsh = prev.oh-my-zsh.overrideAttrs (previousAttrs: {
-      src = previousAttrs.src.override {
-        rev = "418046e9583f635b0303e4b8cf31c356b175cec3";
-        sha256 = "sha256-r36vF37J+3rLGg0QzmT4U8Lp5nqRhAs8We0aDtBJKJM=";
-      };
-    });
   };
 }
