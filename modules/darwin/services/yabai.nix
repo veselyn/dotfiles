cd869fe5ab709e7660736e97b9bdb76c343395fc
Veselin Ivanov <me@veselyn.com>
2024-02-03 22:11:23 +0100

    feat(darwin/services): set yabai padding and gap to 1

--- modules/darwin/services/yabai.nix
+++ modules/darwin/services/yabai.nix
@@ -4,11 +4,11 @@ _: {
     enableScriptingAddition = true;
     config = {
       layout = "bsp";
-      left_padding = 5;
-      bottom_padding = 5;
-      top_padding = 5;
-      right_padding = 5;
-      window_gap = 5;
+      left_padding = 1;
+      bottom_padding = 1;
+      top_padding = 1;
+      right_padding = 1;
+      window_gap = 1;
       mouse_follows_focus = "on";
       window_origin_display = "focused";
       window_shadow = "off";
