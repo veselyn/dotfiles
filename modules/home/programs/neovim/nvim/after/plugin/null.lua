aff4ff1a4edac85480c7b033522484d53e4cfbb9
Veselin Ivanov <me@veselyn.com>
2024-03-22 23:37:21 +0100

    feat(nvim): remove all nullls diagnostics

--- modules/home/programs/neovim/nvim/after/plugin/null.lua
+++ modules/home/programs/neovim/nvim/after/plugin/null.lua
@@ -2,10 +2,6 @@ local null = require("null-ls")
 
 null.setup({
 	sources = {
-		null.builtins.diagnostics.eslint,
-		null.builtins.diagnostics.mypy,
-		null.builtins.diagnostics.pylint,
-		null.builtins.diagnostics.statix,
 		null.builtins.formatting.treefmt,
 	},
 	on_attach = function(_, bufnr)
