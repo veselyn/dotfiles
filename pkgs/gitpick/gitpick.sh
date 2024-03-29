dfccd5b6c625e1994a63a8f9ef2c2ededed523c2
Veselin Ivanov <me@veselyn.com>
2024-03-29 22:39:46 +0100

    refactor(pkgs): rename ref var in gitpick to rev

--- pkgs/gitpick/gitpick.sh
+++ pkgs/gitpick/gitpick.sh
@@ -1,8 +1,8 @@
-ref=$1
+rev=$1
 path=$2
 
-files=$(git ls-tree -r --name-only "$ref" "$path")
+files=$(git ls-tree -r --name-only "$rev" "$path")
 
 for file in $files; do
-	git show "$ref":"$file" >"$file"
+	git show "$rev":"$file" >"$file"
 done
