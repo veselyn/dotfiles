57cec170874822cef565e38a271bbb8f9602ee1f
Veselin Ivanov <me@veselyn.com>
2024-05-31 13:17:32 +0200

    feat(nvim): remove floatborder highlight link to normal
    Unnecessary after the new default colorscheme of Neovim.

--- modules/home/programs/neovim/nvim/colors/tluafed.lua
+++ modules/home/programs/neovim/nvim/colors/tluafed.lua
@@ -3,4 +3,3 @@ vim.cmd.runtime("colors/dim.vim")
 vim.g.colors_name = vim.fn.expand("<sfile>:t:r")
 
 vim.api.nvim_set_hl(0, "SignColumn", {})
-vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
