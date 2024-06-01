41b24c3accf2766d3c33da89f8aa55dbd99839e1
Veselin Ivanov <me@veselyn.com>
2024-06-01 13:44:08 +0200

    feat(darwin/system): increase mouse scaling to 1.0

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,5 @@
 _: {
+  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = 1.0;
   system.defaults.NSGlobalDomain."com.apple.keyboard.fnState" = true;
   system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
   system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
