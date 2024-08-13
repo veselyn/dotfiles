b3a5b84746163747c782121babc0e68b8009b857
Veselin Ivanov <me@veselyn.com>
2024-08-13 17:11:34 +0200

    feat(overlays): recurse into pkgs attrs

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,7 @@
   default = final: prev: let
     stable = import nixpkgs-stable {inherit (prev) system;};
     master = import nixpkgs-master {inherit (prev) system;};
-    pkgs = prev.callPackages ../pkgs {};
+    pkgs = prev.lib.recurseIntoAttrs (prev.callPackages ../pkgs {});
   in {
     inherit stable;
     inherit master;
