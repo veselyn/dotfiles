85c5140e29283682a8edbcd3fdf4102ae0e5c4fc
Veselin Ivanov <me@veselyn.com>
2024-08-17 13:23:03 +0200

    feat(home): clean up user packages

--- modules/home/standalone/packages.nix
+++ modules/home/standalone/packages.nix
@@ -18,50 +18,19 @@ with pkgs; {
     ];
     others = [
       _1password
-      alejandra
-      black
       cachix
-      cargo
-      cargo-modules
-      cargo-watch
-      clippy
-      cloc
       devenv
       fd
-      gcc
-      go
-      gofumpt
-      golangci-lint
-      gotools
-      grc
-      highlight
       httpie
       just
       kubectl
       kubectx
       lazydocker
-      luaPackages.luacheck
-      luajit
-      mypy
-      neofetch
       nix-init
-      nodePackages.eslint
-      nodePackages.prettier
-      nodejs
       parallel
-      pre-commit
-      pylint
-      python3
-      python3Packages.pip
       ripgrep
-      rustc
-      rustfmt
       sd
-      shellcheck
-      shfmt
       silver-searcher
-      statix
-      stylua
       tree
       treefmt
       watch
