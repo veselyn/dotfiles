caa01ef16bfa71c6c56e8d3809df29b663dc198f
Veselin Ivanov <me@veselyn.com>
2024-09-06 21:42:15 +0200

    feat(overlays): expose all pkgs versions

--- overlays/default.nix
+++ overlays/default.nix
@@ -25,17 +25,21 @@
   pkgsUnstable = final: prev: let
     pkgsUnstable = self.lib.mkPkgs nixpkgs-unstable prev.system {overlays = [];};
   in {
+    unstable = pkgsUnstable;
   };
 
   pkgsStable = final: prev: let
     pkgsStable = self.lib.mkPkgs nixpkgs-stable prev.system {overlays = [];};
   in {
+    stable = pkgsStable;
+
     inherit (pkgsStable) kitty;
   };
 
   pkgsMaster = final: prev: let
     pkgsMaster = self.lib.mkPkgs nixpkgs-master prev.system {overlays = [];};
   in {
+    master = pkgsMaster;
   };
 
   agenix = agenix.overlays.default;
