a42e66f7e5f960b34284e9379f61c9d02d6ccdc5
Veselin Ivanov <me@veselyn.com>
2024-09-09 17:36:51 +0200

    flake: initialize flake-parts template

--- flake.nix
+++ flake.nix
@@ -2,8 +2,15 @@
   description = "universe";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
   };
 
-  outputs = inputs: {
-  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+
+      imports = [
+      ];
+    };
 }
