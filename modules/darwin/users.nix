0fcd03d3efdbab5d475154ac15583c384fe4fe53
Veselin Ivanov <me@veselyn.com>
2024-09-10 21:10:32 +0200

    feat(darwin): add master key to veselin's authorized keys

--- modules/darwin/users.nix
+++ modules/darwin/users.nix
@@ -1,9 +1,15 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (pkgs) lib;
   cfg = config.modules.darwin;
 in {
   users.users = {
     ${cfg.user} = {
       home = "/Users/${cfg.user}";
+      openssh.authorizedKeys.keys = [lib.own.sshKeys.master];
     };
   };
 }
