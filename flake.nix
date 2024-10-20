26e3712c11e4c337e680bb8820dffd8a0a56dc72
Veselin Ivanov <me@veselyn.com>
2024-10-20 20:59:42 +0200

    modules: set up flake module outputs

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
 
       imports = [
         ./dev
+        ./modules
       ];
     };
 }
---
+++ modules/default.nix
@@ -0,0 +1,6 @@
+_: {
+  flake.modules.darwin.default = ./darwin;
+  flake.modules.generic.default = ./generic;
+  flake.modules.home.default = ./home;
+  flake.modules.nixos.default = ./nixos;
+}
