64e5ec0d80bf23fed1f6737306fb3e8e7e542a47
Veselin Ivanov <me@veselyn.com>
2024-10-26 13:55:16 +0200

    fix(nvim): run treefmt even when no treefmt.toml exists

--- modules/home/programs/neovim/nvim/after/plugin/null.lua
+++ modules/home/programs/neovim/nvim/after/plugin/null.lua
@@ -1,9 +1,13 @@
 local null = require("null-ls")
 
+local treefmt = null.builtins.formatting.treefmt.with({
+	condition = function()
+		return true
+	end,
+})
+
 null.setup({
-	sources = {
-		null.builtins.formatting.treefmt,
-	},
+	sources = { treefmt },
 	on_attach = function(_, bufnr)
 		vim.api.nvim_exec_autocmds("User", { pattern = "LspOnAttach", data = { bufnr = bufnr } })
 	end,
