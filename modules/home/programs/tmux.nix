bc247a761a917d28e74f0a96e757578eb3d39923
Veselin Ivanov <me@veselyn.com>
2024-10-26 12:43:46 +0200

    fix(home/programs): resolve issues with tmux 3.5a
    Tmux-sensible is executed with run-shell which sets SHELL to /bin/sh
    making the default-command set by it equal to reattach-to-user-namespace /bin/sh
    
    bind-key no longer works with C-X binds and instead requites C-x
    (lowercase x)

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -17,21 +17,23 @@
     ];
 
     extraConfig = ''
+      set -g default-command $SHELL
+
       set -g status off
       set -g renumber-windows on
 
-      bind C-A send C-A
-      bind C-H send C-H
-      bind C-J send C-J
-      bind C-K send C-K
-      bind C-L send C-L
+      bind C-a send C-a
+      bind C-h send C-h
+      bind C-j send C-j
+      bind C-k send C-k
+      bind C-l send C-l
       bind C-\\ send C-\\
 
-      bind c new-window -c "#{pane_current_path}"
-      bind - split-window -c "#{pane_current_path}"
-      bind \\ split-window -h -c "#{pane_current_path}"
-      bind _ split-window -f -c "#{pane_current_path}"
-      bind | split-window -f -h -c "#{pane_current_path}"
+      bind c new-window -c '#{pane_current_path}'
+      bind - split-window -c '#{pane_current_path}'
+      bind \\ split-window -h -c '#{pane_current_path}'
+      bind _ split-window -f -c '#{pane_current_path}'
+      bind | split-window -f -h -c '#{pane_current_path}'
     '';
   };
 
