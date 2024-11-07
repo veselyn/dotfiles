d2de9ed9bc27aae1fc3355b448bdc2a5649e8c18
Veselin Ivanov <me@veselyn.com>
2024-11-07 21:44:30 +0100

    modules/generic: allow unfree nixpkgs

--- modules/generic/default.nix
+++ modules/generic/default.nix
@@ -1,2 +1,17 @@
-_: {
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.self.modules.generic;
+in {
+  options = {
+    self.modules.generic = {
+      enable = lib.mkEnableOption "generic";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = true;
+  };
 }
