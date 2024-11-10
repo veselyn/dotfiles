287f16acf1e5b7986aaca8c530a3b53410f5bcdf
Veselin Ivanov <me@veselyn.com>
2024-11-10 20:01:19 +0100

    modules: add nixpkgs versions to nix registry

--- modules/generic/default.nix
+++ modules/generic/default.nix
@@ -15,6 +15,11 @@ in {
 
   config = lib.mkIf cfg.enable {
     nix.package = pkgs.nix;
+    nix.registry = {
+      nixpkgs-unstable.flake = toplevel.inputs.nixpkgs;
+      nixpkgs-stable.flake = toplevel.inputs.nixpkgs-stable;
+      nixpkgs-master.flake = toplevel.inputs.nixpkgs-master;
+    };
     nix.settings.experimental-features = ["nix-command" "flakes"];
     nixpkgs.config.allowUnfree = true;
     nixpkgs.overlays = builtins.attrValues toplevel.self.overlays;
