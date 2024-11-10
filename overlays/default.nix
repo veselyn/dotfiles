c2b2c95f5769e9df04f46a43d76ba2302bbbd2c8
Veselin Ivanov <me@veselyn.com>
2024-11-10 20:43:14 +0100

    overlays: make all pkgs versions

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,15 @@
-_: {
+{inputs, ...}: let
+  mkPkgs = nixpkgs: system:
+    import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+in {
   flake.overlays = {
-    default = _final: _prev: {
+    default = final: _prev: {
+      unstable = mkPkgs inputs.nixpkgs final.system;
+      stable = mkPkgs inputs.nixpkgs-stable final.system;
+      master = mkPkgs inputs.nixpkgs-master final.system;
     };
   };
 }
