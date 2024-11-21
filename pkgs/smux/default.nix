47b46ed2b0276494434d5c83de529638d3592024
Veselin Ivanov <me@veselyn.com>
2024-11-21 21:13:45 +0100

    pkgs/smux: implement basic switch command

--- pkgs/smux/default.nix
+++ pkgs/smux/default.nix
@@ -2,6 +2,14 @@ _: {
   perSystem = {pkgs, ...}: {
     packages.smux = pkgs.writeShellApplication {
       name = "smux";
+      runtimeInputs = builtins.attrValues {
+        inherit
+          (pkgs)
+          argc
+          fzf
+          tmux
+          ;
+      };
       text = builtins.readFile ./smux.sh;
     };
   };
--- pkgs/smux/smux.sh
+++ pkgs/smux/smux.sh
@@ -1,1 +1,25 @@
+declare -a paths
+paths+=(
+	"${HOME}/dotfiles"
+	"${HOME}/dev/cv"
+	"${HOME}/dev/kv"
+)
 
+# @cmd Switch to a session
+# @meta default-subcommand
+# @arg path
+function switch() {
+	local path
+	path=${argc_path-$(printf "%s\n" "${paths[@]}" | fzf)}
+
+	local session
+	session=$(basename "${path}")
+
+	if ! tmux has-session -t="${session}"; then
+		tmux new-session -d -c "${path}" -s "${session}"
+	fi
+
+	tmux switch-client -t="${session}"
+}
+
+eval "$(argc --argc-eval "$0" "$@")"
