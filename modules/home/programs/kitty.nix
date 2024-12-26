1e85049acfca1595f95fc33baecc643ddbb3d3a0
Veselin Ivanov <me@veselyn.com>
2024-12-26 12:54:29 +0200

    modules/home: link mode specific kitty themes in xdg

--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -1,9 +1,12 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.self.modules.home;
+
+  mkThemePath = theme: "${pkgs.kitty-themes}/share/kitty-themes/themes/${theme}.conf";
 in {
   config = lib.mkIf cfg.enable {
     programs.kitty = {
@@ -24,10 +27,12 @@ in {
       keybindings = {
         "super+f" = "select_tab";
       };
+    };
 
-      extraConfig = ''
-        include theme.conf
-      '';
+    xdg.configFile = {
+      "kitty/dark-theme.auto.conf".source = mkThemePath "Default";
+      "kitty/light-theme.auto.conf".source = mkThemePath "CLRS";
+      "kitty/no-preference-theme.auto.conf".source = mkThemePath "Default";
     };
   };
 }
