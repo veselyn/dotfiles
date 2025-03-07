678eb86d19548860c739257fdd6e457c44df456b
Veselin Ivanov <me@veselyn.com>
2025-03-07 10:09:51 +0100

    modules/home: unify neovim indentline setup

--- modules/home/programs/neovim/nvim/after/plugin/indentline.lua
+++ modules/home/programs/neovim/nvim/after/plugin/indentline.lua
@@ -1,3 +1,14 @@
 local indentline = require("ibl")
+local hooks = require("ibl.hooks")
 
-indentline.setup()
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "IblIndent", { link = "LineNr" })
+	vim.api.nvim_set_hl(0, "IblWhitespace", { link = "LineNr" })
+	vim.api.nvim_set_hl(0, "IblScope", { link = "Whitespace" })
+end)
+
+local char = "▏"
+
+indentline.setup({
+	indent = { char = char, tab_char = char },
+})
--- modules/home/programs/neovim/nvim/plugin/opt.lua
+++ modules/home/programs/neovim/nvim/plugin/opt.lua
@@ -8,7 +8,6 @@ vim.opt.ignorecase = true
 vim.opt.lazyredraw = true
 vim.opt.list = true
 vim.opt.listchars:append({ extends = ">", precedes = "<" })
-vim.opt.listchars:append({ tab = "│ " })
 vim.opt.mousemodel = "extend"
 vim.opt.number = true
 vim.opt.relativenumber = true
