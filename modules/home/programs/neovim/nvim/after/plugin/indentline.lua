a022542b9e72167d5a9554cc83d2347c55009c27
Veselin Ivanov <me@veselyn.com>
2025-07-08 21:41:33 +0200

    vim: move all custom highlights to colorscheme

--- modules/home/programs/neovim/nvim/after/plugin/indentline.lua
+++ modules/home/programs/neovim/nvim/after/plugin/indentline.lua
@@ -1,11 +1,4 @@
 local indentline = require("ibl")
-local hooks = require("ibl.hooks")
-
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-	vim.api.nvim_set_hl(0, "IblIndent", { link = "LineNr" })
-	vim.api.nvim_set_hl(0, "IblWhitespace", { link = "LineNr" })
-	vim.api.nvim_set_hl(0, "IblScope", { link = "Whitespace" })
-end)
 
 local char = "▏"
 
--- modules/home/programs/neovim/nvim/colors/tluafed.lua
+++ modules/home/programs/neovim/nvim/colors/tluafed.lua
@@ -3,3 +3,8 @@ vim.cmd.runtime("colors/dim.vim")
 vim.g.colors_name = vim.fn.expand("<sfile>:t:r")
 
 vim.api.nvim_set_hl(0, "SignColumn", {})
+
+vim.api.nvim_set_hl(0, "IblIndent", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "IblScope", { link = "Whitespace" })
+vim.api.nvim_set_hl(0, "IblWhitespace", { link = "LineNr" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { ctermfg = vim.o.background == "dark" and 15 or 7, fg = "#d65d0e" })
