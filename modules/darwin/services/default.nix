0261beedbf250d2935168958baaba990fcf24d2f
Veselin Ivanov <me@veselyn.com>
2025-07-11 22:56:46 +0200

    modules/darwin: enable dnsmasq service

--- modules/darwin/services/default.nix
+++ modules/darwin/services/default.nix
@@ -1,6 +1,18 @@
-_: {
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.self.modules.darwin;
+in {
   imports = [
     ./skhd.nix
     ./yabai.nix
   ];
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      dnsmasq.enable = true;
+    };
+  };
 }
