02d965fa9061f82beb4940af29460f808594b40d
Veselin Ivanov <me@veselyn.com>
2025-11-16 19:01:45 +0100

    pkgs/opsh: add support for multiple args command

--- pkgs/opsh/opsh.sh
+++ pkgs/opsh/opsh.sh
@@ -3,10 +3,10 @@
 # @meta require-tools op
 # @flag -m --mask
 # @option -e --env*
-# @arg cmd
+# @arg cmd*
 function main() {
 	local mask=${argc_mask-0}
-	local cmd=${argc_cmd-${SHELL}}
+	local cmd=("${argc_cmd[@]-${SHELL}}")
 
 	local -a envs
 
@@ -24,7 +24,7 @@ function main() {
 		args+=("--env-file" "${env}")
 	done
 
-	SHLVL=$((SHLVL - 1)) op run "${args[@]}" -- "${cmd}"
+	SHLVL=$((SHLVL - 1)) op run "${args[@]}" -- "${cmd[@]}"
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
