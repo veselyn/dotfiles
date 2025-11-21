817001678dcaf5ed77fbe4efdd0154e29749c825
Veselin Ivanov <me@veselyn.com>
2025-11-21 10:31:51 +0100

    vim: disable frontmatter and configure daily notes folder for obsidian

--- modules/home/programs/neovim/nvim/after/plugin/obsidian.lua
+++ modules/home/programs/neovim/nvim/after/plugin/obsidian.lua
@@ -1,6 +1,8 @@
 local obsidian = require("obsidian")
 
 obsidian.setup({
+	daily_notes = { folder = "0 INBOX" },
+	frontmatter = { enabled = false },
 	legacy_commands = false,
 	ui = { enable = false },
 	workspaces = { { path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault" } },
