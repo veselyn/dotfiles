21ba9b5a78af1e0bd59d6acbeb794c9357331dc4
Veselin Ivanov <me@veselyn.com>
2026-03-02 16:57:07 +0100

    modules/home: add homelab entries to ssh config

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -9,6 +9,16 @@ in {
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
+
+      matchBlocks =
+        lib.genAttrs ["pve01" "pve02" "pve03" "pve04" "pve05"] (node: {
+          hostname = "${node}.homelab.veselabs.com";
+          user = "root";
+        })
+        // lib.genAttrs ["master01" "master02" "master03" "worker01" "worker02"] (node: {
+          hostname = "${node}.kubernetes.homelab.veselabs.com";
+          user = "debian";
+        });
     };
   };
 }
