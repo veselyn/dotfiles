11fc66072ca4e75671823cd634902c62f9299df5
Veselin Ivanov <me@veselyn.com>
2026-05-12 16:53:06 +0200

    modules/home: remove pve04, pve05, worker01, and worker02 from ssh config

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -11,11 +11,11 @@ in {
       enableDefaultConfig = false;
 
       matchBlocks =
-        lib.genAttrs ["pve01" "pve02" "pve03" "pve04" "pve05"] (node: {
+        lib.genAttrs ["pve01" "pve02" "pve03"] (node: {
           hostname = "${node}.homelab.veselabs.com";
           user = "root";
         })
-        // lib.genAttrs ["master01" "master02" "master03" "worker01" "worker02"] (node: {
+        // lib.genAttrs ["master01" "master02" "master03"] (node: {
           hostname = "${node}.kubernetes.homelab.veselabs.com";
           user = "debian";
         });
